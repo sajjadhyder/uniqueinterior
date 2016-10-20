af57973648ef62f4cc3b3a54526a397d37de5a9e
sajjadhyder <mr.sajjad.hyder@gmail.com>
2016-10-20 16:36:09 +0530

    initial website

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html lang="en" class="no-js ie6"> <![endif]-->
+<!--[if IE 7]>    <html lang="en" class="no-js ie7"> <![endif]-->
+<!--[if IE 8]>    <html lang="en" class="no-js ie8"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class='no-js' lang='en'>
+    <!--<![endif]-->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible' />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Unique interior middle east - Coming Soon</title> 
+        <meta name="description" content="Unique interior middle east"/>
+        <!-- Open Graph data -->
+        <meta property="og:site_name" content="Aroma 24/7">
+        <meta property="og:title" content="Aroma 24/7" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.uniqueinteriorme.com/" />
+        <meta property="og:image" itemprop="image" content="http://www.uniqueinteriorme.com/images/product.jpg" />
+        <meta property="og:description" content="Unique interior middle east" />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="630" />
+
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/app.css" type="text/css" media="screen" charset="utf-8" />
+<link href="https://fonts.googleapis.com/css?family=Cinzel|Fugaz+One|Raleway:600" rel="stylesheet">
+
+        <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+        
+        <!--[if IE 6]>
+            <style type="text/css" media="screen">
+                .gradient {display:none;}
+            </style>
+        <![endif]-->
+    </head>
+    <body>
+
+
+        <!-- Site Logo -->
+    <div id="content">
+        <div class="text-center">
+            <div id="logo" >
+            
+            <h1 >Unique </h1>
+            <h2>Interior and Furniture industry</h2>
+            </div>
+        </div>
+
+        <div class= "container">
+            <div class="row text-block">
+                <div class=" col-sm-12 text-center"  >
+                
+                    <p style="font-size:4em;">Our new website is under construction!</p>
+                    
+                </div>
+            </div>
+        </div>
+    
+        <!-- Contact Page -->
+    
+
+        
+        <div id= "contact" class="container">
+            <div class ="row">
+                
+                <div class="col-sm-12 text-center">
+                    <h2 >Our contact details</h2>
+                    <h4>Get in touch with us!</h4>
+                </div>
+
+                    <div class=" col-lg-4 col-md-2"></div>
+                    <div class=" col-lg-5 col-sm-12 col-md-8">
+                        <p class="text-center contact-details">Unique interior<br/>Email: <a href="mailto:info@uniqueinteriorme.com">info@uniqueinteriorme.com</a><br />Phone: <a href="tel:+97142860960">+971 4 286 09 60</a><br />P.O. Box: 35473<br/>Dubai<br />United Arab Emirates</p>
+                    </div>
+                    <div class=" col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+        </div>
+
+        
+        <script  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js"></script>
+        
+        <script type="text/javascript" charset="utf-8">
+            $("body").backstretch([
+            "/images/backgrounds/bg-img-1.jpg"
+            , "/images/backgrounds/bg-img-2.jpeg"
+            , "/images/backgrounds/bg-img-3.jpg", "/images/backgrounds/bg-img-4.jpg", "/images/backgrounds/bg-img-5.jpeg"
+            ], {duration: 4000, fade: 750});
+        </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56528360-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+  </body>
+</html>
